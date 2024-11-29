2e342e39230255650ba66a66b124883922486cb4
petrickad <petrick.duarte99@gmail.com>
2024-11-28 22:52:35 -0300

    finalizando o projeto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+    
+    window.addEventListener('scroll', function(){
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < alturaHero){
+            hideHeaderElements() ;
+        }
+        else{
+            showHeaderElements();
+        }
+    })
+
+    //Seção de atrações, programação das abas
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
             const abaAlvo = button.target.dataset.tabButton;
@@ -13,7 +29,32 @@ document.addEventListener('DOMContentLoaded', function(){
             button.target.classList.add('shows__tabs__button--is--active');
         })
     }
+
+    //Seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openOrCloseQuestion);
+    }
 })
+
+function hideHeaderElements(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hiden');
+
+}
+
+function showHeaderElements(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hiden');
+
+}
+
+function openOrCloseQuestion(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log();
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}   
 
 function removeButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
